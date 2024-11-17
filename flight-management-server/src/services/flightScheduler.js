6e7cde80450d1cf39a3eb5734e5eef758f17651a
Usman Najmi <50474705+Usman0455961@users.noreply.github.com>
2024-11-17 16:03:30 +0500

    chore:remove unecessary file

--- flight-management-server/src/services/flightScheduler.js
+++
@@ -1,9 +0,0 @@
-const { updateRandomFlightsStatus } = require('./flightService');
-
-function initializeFlightScheduler() {
-    // Update random flight status every 30 seconds
-    setInterval(updateRandomFlightsStatus, 5000);
-    console.log('Flight scheduler initialized');
-}
-
-module.exports = initializeFlightScheduler; 
